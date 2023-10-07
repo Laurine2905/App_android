933f21c40d9ce2dabd4d9161818914a5f5604c06
Laurine2905 <laurine.rat@outlook.fr>
2023-10-07 12:01:42 +0200

    style okay

--- app/src/main/java/laurine/isis/fr/MyCart.kt
+++ app/src/main/java/laurine/isis/fr/MyCart.kt
@@ -3,14 +3,22 @@ package laurine.isis.fr
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 
 @Composable
 fun MyCard(
@@ -25,13 +33,24 @@ fun MyCard(
             .fillMaxWidth()
             .padding(15.dp)
     ) {
-        Column {
-            AsyncImage(
-                model = "https://image.tmdb.org/t/p/w500$chemin_img",
-                contentDescription = titre
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp))
+        {
+            androidx.compose.foundation.Image(
+                painter = rememberImagePainter(
+                    data = "https://image.tmdb.org/t/p/w500$chemin_img",
+
+                ),
+                contentDescription = titre,
+                modifier = Modifier
+                    .size(180.dp)
+                    .align(Alignment.CenterHorizontally)
             )
             Text(
                 text = titre,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
                 modifier = Modifier
