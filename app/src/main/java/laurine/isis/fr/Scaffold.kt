d177bdedcdf3caee203919ff9a8c6e6714dda295
Laurine2905 <laurine.rat@outlook.fr>
2023-10-21 15:48:02 +0200

    paysage recherche

--- app/src/main/java/laurine/isis/fr/Scaffold.kt
+++ app/src/main/java/laurine/isis/fr/Scaffold.kt
@@ -1,9 +1,10 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,23 +13,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
@@ -36,15 +33,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.SemanticsProperties.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import laurine.isis.fr.Film
 import laurine.isis.fr.MainViewModel
@@ -257,6 +252,62 @@ fun BottomAppBarExample(
                     else -> Text(text = "Contenu inconnu")
                 }
             }
+            // Bouton rond en bas à droite
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.End
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .background(Color.Gray, CircleShape)
+                        .clickable {
+                            // Action lorsque le bouton rond est cliqué
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    IconButton(onClick = {
+                        searchVisible = !searchVisible
+                        // Réinitialiser la recherche lorsque le champ est masqué
+                        if (!searchVisible) {
+                            searchQuery = ""
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search Icon"
+                        )
+                    }
+
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.Start
+            ) {
+            if (searchVisible) {
+                // Champ de texte pour la recherche
+                TextField(
+                    value = searchQuery,
+                    onValueChange = {
+                        searchQuery = it
+                        // Appeler votre fonction de recherche ici avec la query
+                        when (value) {
+                            "series" -> viewModel.searchSeries(searchQuery)
+                            "films" -> viewModel.searchMovies(searchQuery)
+                            "acteurs" -> viewModel.searchPersons(searchQuery)
+                        }
+                    },
+                    placeholder = { Text("Rechercher") },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
         }
     }
-}
+}}
